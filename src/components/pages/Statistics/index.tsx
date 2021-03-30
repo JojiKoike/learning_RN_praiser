a4d5ff4510ae27aa546a643d1fdd20464bc8ccc8
JojiKoike <joji.koike@gmail.com>
2021-03-30 22:23:16 +0900

    :+1: Embed Navigation for Detail

--- src/components/pages/Statistics/index.tsx
+++ src/components/pages/Statistics/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { DETAIL } from '../../../constants/path';
 
 const Statistics = () => {
+  const { navigate } = useNavigation();
   return (
     <View style={styles.container}>
       <Text>Statistics</Text>
+      <TouchableOpacity onPress={() => navigate(DETAIL)}>
+        <Text>Go to Detail</Text>
+      </TouchableOpacity>
     </View>
   );
 };
