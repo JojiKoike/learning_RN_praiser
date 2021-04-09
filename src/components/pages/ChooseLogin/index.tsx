84fc1f3c4d2b8960cabe7f196e4b85a3e26bb037
JojiKoike <joji.koike@gmail.com>
2021-04-09 23:15:51 +0900

    :+1: Sign In and Sign Up

--- src/components/pages/ChooseLogin/index.tsx
+++ src/components/pages/ChooseLogin/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { SIGN_IN, SIGN_UP } from '../../../constants/path';
 
 const ChooseLogin = () => {
+  const { navigate } = useNavigation();
   return (
     <View style={styles.container}>
-      <Text>ChooseLogin</Text>
+      <Text>Choose Login</Text>
+      <TouchableOpacity onPress={() => navigate(SIGN_IN)}>
+        <Text>go to Sign in</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigate(SIGN_UP)}>
+        <Text>go to Sign up</Text>
+      </TouchableOpacity>
     </View>
   );
 };
