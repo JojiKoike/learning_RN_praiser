0f24be77d42ed98daac3ffc44fbb3d50120cc496
JojiKoike <joji.koike@gmail.com>
2021-04-09 22:29:39 +0900

    :+1: Sign In Page

---
+++ src/components/pages/SignIn/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Context, Status } from '../../../contexts/ui';
+
+const SignIn = () => {
+  const { setApplicationState } = React.useContext(Context);
+  return (
+    <View style={styles.container}>
+      <Text>SignIn</Text>
+      <TouchableOpacity onPress={() => setApplicationState(Status.AUTHORIZED)}>
+        <Text>Sign In</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default SignIn;
