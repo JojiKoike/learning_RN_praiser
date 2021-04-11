03fc6e41b610e0c644658c1c31fc5090c030e457
JojiKoike <joji.koike@gmail.com>
2021-04-12 02:41:14 +0900

    :+1: Apply Theme Color

--- src/components/pages/Loading/index.tsx
+++ src/components/pages/Loading/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import * as UiContext from '../../../contexts/ui';
+import { COLOR } from '../../../constants/theme';
 
 const ChangeStateButton = (props: { state: UiContext.Status }) => {
   const { setApplicationState } = React.useContext(UiContext.Context);
   const { state } = props;
   return (
     <TouchableOpacity onPress={() => setApplicationState(state)}>
-      <Text>Change State to {state}</Text>
+      <Text style={styles.text}>Change State to {state}</Text>
     </TouchableOpacity>
   );
 };
@@ -27,6 +28,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: COLOR.MAIN,
+  },
+  text: {
+    color: COLOR.WHITE,
   },
 });
 
