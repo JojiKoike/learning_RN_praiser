972b947f96ac20a2e177928a762100acacb016f1
JojiKoike <joji.koike@gmail.com>
2021-04-18 23:14:19 +0900

    :+1: UI : SignIn

---
+++ src/components/molecules/SignInWithGoogle.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Button from '../atoms/Button';
+import { Context, Status } from '../../contexts/ui';
+
+const SignInWithGoogle = () => {
+  const { setApplicationState } = React.useContext(Context);
+  return <Button onPress={() => setApplicationState(Status.AUTHORIZED)} icon="google" label="Sign In with Google" />;
+};
+
+export default SignInWithGoogle;
