acf1dfe849a0a79d8f887aab038f450fbb3abcfb
JojiKoike <joji.koike@gmail.com>
2021-04-18 11:43:44 +0900

    :+1: Logo

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Image, ImageSourcePropType, StyleSheet, ImageStyle } from 'react-native';
+import { width } from '../../lib/window';
+import reactImage from '../../../assets/reactIcon.jpg';
+
+const edgeNumber = 2;
+const ratio = 3;
+const imageRatio = edgeNumber / ratio;
+
+interface Props {
+  image?: ImageSourcePropType;
+  style?: ImageStyle | ImageStyle[];
+}
+
+const Logo = (props: Props) => {
+  const { image = reactImage, style } = props;
+  return <Image source={image} resizeMode="contain" style={[styles.image, style]} />;
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: width * imageRatio,
+    flex: 1,
+    resizeMode: 'contain',
+  },
+});
+
+export default Logo;
