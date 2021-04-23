fce894f4ca7238d84d7287ce3e9093e5a303dcb9
JojiKoike <joji.koike@gmail.com>
2021-04-23 22:24:58 +0900

    :+1: Implemented Carousel Initial

--- src/components/pages/Initial/index.tsx
+++ src/components/pages/Initial/index.tsx
@@ -1,20 +1,63 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useCallback, useContext, useRef, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import CarouselStatic from 'react-native-snap-carousel';
+import SafeAreaView from 'react-native-safe-area-view';
+import { Carousel } from '../../organisnms';
+import { Pagination } from '../../atoms';
 import { COLOR } from '../../../constants/theme';
+import { Context, Status } from '../../../contexts/ui';
+
+interface Data {
+  text: string;
+}
+
+const renderData = [
+  {
+    text: 'Hello React Native world ¥nWelcome to the JS World.¥n¥n This application is made from React Native.',
+  },
+  {
+    text: 'If you use this application, ¥nyou could manage your task.¥n¥nSo, please register and manage your task.',
+  },
+  {
+    text: 'In the first, you have to register your account.¥n¥nPlease, tell me your email.',
+  },
+];
 
 const Initial = () => {
+  const [activeSlide, changeSlide] = useState(0);
+  const { setApplicationState } = useContext(Context);
+
+  const carouselRef = useRef(null);
+  const onEnd = useCallback(() => {
+    setApplicationState(Status.UN_AUTHORIZED);
+  }, [setApplicationState]);
+  const onNext = useCallback(() => {
+    const nextIndex = activeSlide === renderData.length - 1 ? activeSlide : 1 + activeSlide;
+    setTimeout(() => {
+      if (!carouselRef || !carouselRef.current) {
+        return;
+      }
+      const carousel = (carouselRef.current as any) as CarouselStatic<Data>;
+      carousel.snapToItem(nextIndex);
+    }, 250);
+    changeSlide(nextIndex);
+  }, [activeSlide]);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Initial</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Carousel data={renderData} onEnd={onEnd} onNext={onNext} carouselRef={carouselRef} onSnapToItem={changeSlide} />
+      <Pagination length={renderData.length} index={activeSlide} />
+    </SafeAreaView>
   );
 };
 
+const padding = 20;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding,
     backgroundColor: COLOR.MAIN,
   },
   text: {
