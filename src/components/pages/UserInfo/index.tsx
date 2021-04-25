b70240d3523a6963293bab8b1a03acfafc1c22a8
JojiKoike <joji.koike@gmail.com>
2021-04-25 23:32:45 +0900

    :+1: UserInfo Page

--- src/components/pages/UserInfo/index.tsx
+++ src/components/pages/UserInfo/index.tsx
@@ -1,25 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Context, Status } from '../../../contexts/ui';
-
-const UserInfo = () => {
-  const { setApplicationState } = React.useContext(Context);
-  return (
-    <View style={styles.container}>
-      <Text>UserInfo</Text>
-      <TouchableOpacity onPress={() => setApplicationState(Status.UN_AUTHORIZED)}>
-        <Text>Sign out</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+import { StyleSheet, Text, View } from 'react-native';
+import { COLOR } from '../../../constants/theme';
+import { Context as UiContext, Status } from '../../../contexts/ui';
+import { Button, Avatar, LabelValueContainer } from '../../atoms';
+import formatDate from '../../../lib/format-date';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
+  imageIconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  nameText: {
+    color: COLOR.WHITE,
+    fontSize: 20,
+    marginTop: 5,
+  },
+  button: {
+    marginTop: 30,
+  },
 });
+
+const userState = {
+  name: 'test',
+  createdAt: '2020/01/01',
+  mailAddress: 'test@rn.com',
+};
+
+const UserInfo = () => {
+  const { setApplicationState } = React.useContext(UiContext);
+  const signOut = React.useCallback(async () => {
+    setApplicationState(Status.UN_AUTHORIZED);
+  }, [setApplicationState]);
+
+  const source = React.useMemo(() => require('../../../../assets/gopher.png'), []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageIconContainer}>
+        <Avatar source={source} />
+        <Text style={styles.nameText}>{userState.name}</Text>
+      </View>
+      <LabelValueContainer label="e-mail" value={userState.mailAddress} />
+      <LabelValueContainer
+        label="registeredAt"
+        value={userState.createdAt && formatDate(new Date(userState.createdAt))}
+      />
+      <Button style={styles.button} onPress={signOut} label="Sign Out" />
+    </View>
+  );
+};
 
 export default UserInfo;
