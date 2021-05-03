8c20a05b94be2f650bf57d8c78c0e0773c23fed0
JojiKoike <joji.koike@gmail.com>
2021-05-03 13:25:29 +0900

    :+1: Network Context

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,19 @@ import { Snackbar } from 'react-native-paper';
 
 import store from './store';
 import * as UiContext from './contexts/ui';
+import * as NetworkContext from './contexts/network';
 import Routes from './routes';
 import ErrorPanel from './components/molecules/ErrorPanel';
+import NetworkPanel from './components/molecules/NetworkPanel';
 
 const App = () => {
   const [applicationState, setApplicationState] = React.useState(UiContext.createApplicationInitialState());
   const [error, setError] = React.useState(UiContext.createErrorInitialState());
   const [snackbar, setSnackbar] = React.useState(UiContext.createSnackbarInitialState());
+  const [networkState, dispatchNetworkActions] = React.useReducer(
+    NetworkContext.reducer,
+    NetworkContext.createInitialState(),
+  );
   const onDismiss = React.useCallback(() => {
     setSnackbar(UiContext.createSnackbarInitialState());
   }, []);
@@ -22,15 +28,18 @@ const App = () => {
         <UiContext.Context.Provider
           value={{ error, setError, snackbar, setSnackbar, applicationState, setApplicationState }}
         >
-          <Routes />
-          <ErrorPanel />
-          <Snackbar
-            visible={snackbar.visible}
-            onDismiss={onDismiss}
-            action={{ label: snackbar.label, onPress: onDismiss }}
-          >
-            {snackbar.message}
-          </Snackbar>
+          <NetworkContext.Context.Provider value={{ networkState, dispatchNetworkActions }}>
+            <Routes />
+            <NetworkPanel />
+            <ErrorPanel />
+            <Snackbar
+              visible={snackbar.visible}
+              onDismiss={onDismiss}
+              action={{ label: snackbar.label, onPress: onDismiss }}
+            >
+              {snackbar.message}
+            </Snackbar>
+          </NetworkContext.Context.Provider>
         </UiContext.Context.Provider>
       </SafeAreaProvider>
     </Provider>
